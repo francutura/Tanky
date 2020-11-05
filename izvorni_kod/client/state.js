8bf053d80c74dca2885faee3bfaa36e745b47eb4
Fran Čutura <fran.cutura@fer.hr>
2020-11-05 16:29:58 +0100

    Hotfix: name rendering

--- izvorni_kod/client/state.js
+++ izvorni_kod/client/state.js
@@ -1,4 +1,4 @@
-const me = {x: 0, y: 0, BODY_ANGLE: 0, CANNON_ANGLE: 0};
+const me = {x: 0, y: 0, username:"None", BODY_ANGLE: 0, CANNON_ANGLE: 0};
 const others = {}
 
 function setState(update){
@@ -18,6 +18,7 @@ function setMyState(update){
 	let main = update['me']
 	me.x = main['x'];
 	me.y = main['y'];
+	me.username = main['username'];
 	me.BODY_ANGLE = main['bodya'];
 	me.CANNON_ANGLE = main['cannona'];
 }
