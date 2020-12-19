b756cc5902635017009e6c654a72d826f9b53e7b
Fran Čutura <fran.cutura@fer.hr>
2020-12-19 16:33:32 +0100

    Add asset

--- izvorni_kod/client/manageAssets.js
+++ izvorni_kod/client/manageAssets.js
@@ -1,4 +1,4 @@
-const _ASSETS = ["boom.svg", "bullet.png", "kugla.svg", "M1A.svg", "M1A_top.svg", "matilda.svg", "matilda_top.svg", "pl-01.svg", "pl-01_top.svg", "sherman.svg", "sherman_top.svg", "t-34.svg", "t34_top.svg", "tankBase.png", "tankTurret.png", "tiger_131.svg", "tiger_131_top.svg", "pinkTile.png", "blackTile.png"];
+const _ASSETS = ["boom.svg", "bullet.png", "kugla.svg", "M1A.svg", "M1A_top.svg", "matilda.svg", "matilda_top.svg", "pl-01.svg", "pl-01_top.svg", "sherman.svg", "sherman_top.svg", "t-34.svg", "t-34_top.svg", "tankBase.png", "tankTurret.png", "tiger_131.svg", "tiger_131_top.svg", "pinkTile.png", "blackTile.png"];
 const assets = {}
 
 
